38fc5f5c7e39444b0152a27e8a461cc4c3b4e90c
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-02-06 22:28:40 -0300

    metodos maratona de filmes java

---
+++ JAVA/aula6/MaratonaFilmes.java
@@ -0,0 +1,112 @@
+//Gerenciador de Maratona de Filmes 
+
+package aula6;
+
+import java.util.Scanner;
+
+public class MaratonaFilmes {
+
+    //==============MÉTODO CABEÇALHO====================
+
+    public static void exibirCabecalho(){
+        System.out.println();
+        System.out.printf("Olá, bem vido ao site Maratona Filmes!");
+        System.out.println();
+        System.out.println();
+        
+    }
+
+
+    //=============Método calcularTempoTotal==================
+
+    public static double calcularTempoTotal(int quantidadeFilmes, double duracaoFilmes){
+        return quantidadeFilmes * duracaoFilmes;
+    }
+
+
+    //===============Método classificarMaratona==============
+
+    public static void classificarMaratona
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.printf("Olá, bem vido ao site Maratona Filmes!");
+        System.out.println();
+        System.out.println();
+        
+
+        //==============VARIÁVEIS=============
+
+        String usuario;
+        double duracaoFilmes = 1.5;
+        int quantidadeFilmes = 0;
+        double resultado;
+        double horas = 0;
+        String classificacao;
+
+        //===========ENTRADA===========
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        usuario = in.nextLine();
+
+        System.out.println("Digite a quantidade de filmes que deseja assistir");
+        quantidadeFilmes = in.nextInt();
+
+        System.out.println("Digite quantas horas tem cada filme");
+        duracaoFilmes = in.nextDouble();
+
+        
+
+        //===========CONVERSÃO=============
+
+        resultado = quantidadeFilmes * duracaoFilmes;
+        horas = (double) quantidadeFilmes;
+
+        if(horas <= 4){
+            classificacao = "Maratona Leve";
+        }else if(horas  <= 8){
+            classificacao = "Maratona Moderada";
+        }else {
+            classificacao = "Maratona Épica";
+        }
+
+
+        //==============SAÍDA===============
+
+        System.out.println();
+        System.out.println("====================================");
+        System.out.println("GERENCIADOR DE MARATONA DE FILMES");
+        System.out.println("====================================");
+        System.out.println();
+
+        System.out.println("Informações da Maratona:");
+        System.out.println();
+
+        System.out.println("Usuário: "+usuario);
+
+        System.out.println("Filmes planejados: "+quantidadeFilmes);
+        
+        System.out.println("Duração média por filme: "+duracaoFilmes);
+
+        System.out.println();
+
+        System.out.println("Cálculos:");
+        System.out.println();
+
+        System.out.println("Tempo total: " + resultado + " horas");
+
+        System.out.println("Classificação: "+classificacao);
+
+       
+
+
+    }
+    
+}
