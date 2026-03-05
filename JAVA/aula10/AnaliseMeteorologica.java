cfa291b137bc99340a610591d65f3389dbeed94f
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-03-04 22:14:25 -0300

    media ponderada

--- JAVA/aula10/AnaliseMeteorologica.java
+++ JAVA/aula10/AnaliseMeteorologica.java
@@ -3,13 +3,17 @@ package aula10;
 import java.util.Scanner;
 
 public class AnaliseMeteorologica {
+
+    //============= método calcularMediaPonderadaTemperatura ========================
     
-    public static void calcularMediaPonderadaTemperatura(double max, double min) {
+    // public static void calcularMediaPonderadaTemperatura(double max, double min) {
 
-       
+    //     double calculo = (max * 0.7) + (min * 0.3);
+    //     return calculo;
         
+    // }
 
-    }
+    //================= método classificarClima ====================================
 
     public static void classificarClima(double tempMedia, int umidadeMedia){
 
@@ -27,8 +31,9 @@ public class AnaliseMeteorologica {
        }
 
 
-
     }
+
+    //==================== método identificarCidadeComMaiorAmplitudeTermica ===============
 
     public static void identificarCidadeComMaiorAmplitudeTermica(){
 
@@ -39,20 +44,66 @@ public class AnaliseMeteorologica {
 
     public static void main(String[] args) {
 
+        // Temperaturas: [máxima, mínima] para 5 cidades
+        double[][] temperaturas = {
+            {32.5, 22.1},  // Cidade 1
+            {28.3, 18.7},  // Cidade 2
+            {35.8, 24.9},  // Cidade 3
+            {30.2, 20.5},  // Cidade 4
+            {25.7, 15.3}   // Cidade 5
+        };
+
+        // Umidades: [manhã, tarde, noite] para 5 cidades
+        int[][] umidades = {
+            {85, 60, 75},  // Cidade 1
+            {78, 55, 70},  // Cidade 2
+            {90, 65, 80},  // Cidade 3
+            {82, 58, 72},  // Cidade 4
+            {75, 50, 68}   // Cidade 5
+        };
+
+        //========== Imprimir dados ============
+
+        //========== temperartura media =========
+        double media;
+
+       //cidade 1
+       
+       media = temperaturas [0][0] * 0.7 + temperaturas [0][1] * 0.3;
+       
+       System.out.printf("%.2f\n", media);
+
+       //cidade2 
+
+       media = temperaturas [1][0] * 0.7 + temperaturas [1][1] * 0.3;
+
+       System.out.printf("%.2f\n", media);
+
+       //cidade 3
+
+       media = temperaturas [2][0] * 0.7 + temperaturas [2][1] * 0.3;
+
+       System.out.printf("%.2f\n", media);
+
+       //cidade 4
+
+       media = temperaturas [3][0] * 0.7 + temperaturas [3][1] * 0.3;
+
+       System.out.printf("%.2f\n", media);
+
+       //cidade 5
+
+       media = temperaturas [4][0] * 0.7 + temperaturas [4][1] * 0.3;
+
+       System.out.printf("%.2f\n", media);
+
+       
 
 
-        
+        // Amplitude
 
-
-        
-
-        
-        
-
-        
-
-        
-
+        double[] amplitude;
+       
 
 
         
