55c4a1039eec21151f20b6ae682b0ed2a0ae72fa
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-03-16 19:35:09 -0300

    extrai exibição do menu para o método void

--- JAVA/MiniBanco/MiniBanco.java
+++ JAVA/MiniBanco/MiniBanco.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 
 public class MiniBanco {
 
+    static void exibirMenu(){
+        System.out.println("\n=== MiniBanco===");
+        System.out.println("1 - Depositar");
+        System.out.println("2 - Sacar");
+        System.out.println("3 - Consultar saldo");
+        System.out.println("4 - Ver extrato");
+        System.out.println("0 - sair");
+        System.out.println("Escolha");
+    }
+
+
     public static void main(String[] args) {
-        
+
         System.out.println("Mini Bnaco iniciado. ");
 
         Scanner scanner = new Scanner(System.in);
@@ -17,6 +28,8 @@ public class MiniBanco {
         System.out.print("Digite o seu nome: ");
         String nome = scanner.next();
         System.out.printf("Olá, %s! Saldo inicial: R$ %.2f\n", nome, saldo);
+
+        exibirMenu();
 
         scanner.close();
         
