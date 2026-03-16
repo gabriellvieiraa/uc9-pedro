712586ac0465a9ebb473ffd84c52d80324cedbcc
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-03-16 19:29:40 -0300

    Feat: adiciona leitura de nome e variáveis da conta

--- JAVA/MiniBanco/MiniBanco.java
+++ JAVA/MiniBanco/MiniBanco.java
@@ -1,10 +1,26 @@
 package MiniBanco;
+import java.util.Scanner;  
 
 public class MiniBanco {
 
     public static void main(String[] args) {
         
         System.out.println("Mini Bnaco iniciado. ");
+
+        Scanner scanner = new Scanner(System.in);
+        
+        //variáveis principais da conta
+        double saldo = 0.0;
+        int opcao = -1;
+
+        //boas vindas
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.next();
+        System.out.printf("Olá, %s! Saldo inicial: R$ %.2f\n", nome, saldo);
+
+        scanner.close();
+        
+
     }
     
 }
